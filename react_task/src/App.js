4516eda108196ec7738fad60c6a2e332cdc7fd92
Tilacin <tilacin45@gmail.com>
2023-02-12 20:28:57 +0300

    начало приложения расходы

---
+++ react_task/src/App.js
@@ -0,0 +1,44 @@
+import CostItem from "./components/CostItem";
+
+const costs = [
+  {
+    date: new Date(2021, 2, 12),
+    description: "Холодильник",
+    amount: 999.99,
+  },
+  {
+    date: new Date(2021, 11, 25),
+    description: "MacBook",
+    amount: 1254.72,
+  },
+  {
+    date: new Date(2021, 4, 1),
+    description: "Джинсы",
+    amount: 49.99,
+  },
+];
+
+function App() {
+  return (
+    <div>
+      <h1>Начнём изучение React!</h1>
+      <CostItem
+        date={costs[0].date}
+        description={costs[0].description}
+        amount={costs[0].amount}
+      ></CostItem>
+      <CostItem
+        date={costs[1].date}
+        description={costs[1].description}
+        amount={costs[1].amount}
+      ></CostItem>
+      <CostItem
+        date={costs[2].date}
+        description={costs[2].description}
+        amount={costs[2].amount}
+      ></CostItem>
+    </div>
+  );
+}
+
+export default App;
