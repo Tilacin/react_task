1dc4c939bf461636c3cc4bcc43beb69eada45c08
Tilacin <tilacin45@gmail.com>
2023-02-21 11:03:51 +0300

    Переходим к разделу 4

--- react_task/src/App.js
+++ react_task/src/App.js
@@ -1,41 +1,43 @@
+import React, { useState } from "react";
 import NewCost from "./components/NewCost/NewCost";
 import Costs from "./components/Costs/Costs";
 
-function App() {
-const costs = [
+const INITIAL_COSTS = [
   {
-    id: 'c1',
+    id: "c1",
     date: new Date(2021, 2, 12),
     description: "Холодильник",
     amount: 999.99,
   },
   {
-    id: 'c2',
+    id: "c2",
     date: new Date(2021, 11, 25),
     description: "MacBook",
     amount: 1254.72,
   },
   {
-    id: 'c3',
+    id: "c3",
     date: new Date(2021, 4, 1),
     description: "Джинсы",
     amount: 49.99,
   },
 ];
 
-const addCostHandler = (cost) => {
-  console.log(cost)
-  console.log('App Component')
-}
+const App = () => {
+  const [costs, setCosts] = useState(INITIAL_COSTS);
 
-return (
-  <div>
-   
-    <NewCost onAddCost={addCostHandler} />
-  <Costs costs={costs}/>
-  </div>
-)
+  const addCostHandler = (cost) => {
+    setCosts((prevCosts) => {
+      return [cost, ...prevCosts];
+    });
+  };
 
-}
+  return (
+    <div>
+      <NewCost onAddCost={addCostHandler} />
+      <Costs costs={costs} />
+    </div>
+  );
+};
 
 export default App;
--- react_task/src/components/Costs/Costs.js
+++ react_task/src/components/Costs/Costs.js
@@ -1,31 +1,30 @@
 import CostItem from "./CostItem";
-import './Costs.css'
+import "./Costs.css";
 import Card from "../UI/Card";
 import CostsFilter from "./CostsFilter";
+import React, { useState } from "react";
 
-function Costs (props) {
-    return(
+const Costs = (props) => {
+  const [selectedYear, setSelectedYear] = useState("2021");
+
+  const yearChangeHandler = (year) => {
+    setSelectedYear(year);
+  };
+
+  return (
     <div>
-      <CostsFilter/>
-    <Card className="costs">
-    <CostItem
-      date={props.costs[0].date}
-      description={props.costs[0].description}
-      amount={props.costs[0].amount}
-    />
-    <CostItem
-      date={props.costs[1].date}
-      description={props.costs[1].description}
-      amount={props.costs[1].amount}
-    />
-    <CostItem
-      date={props.costs[2].date}
-      description={props.costs[2].description}
-      amount={props.costs[2].amount}
-    />
-    </Card>
+      <Card className="costs">
+        <CostsFilter year={selectedYear} onChangeYear={yearChangeHandler} />
+        {props.costs.map((cost) => (
+          <CostItem
+            date={cost.date}
+            description={cost.description}
+            amount={cost.amount}
+          />
+        ))}
+      </Card>
     </div>
-    )
-}
+  );
+};
 
-export default Costs
+export default Costs;
